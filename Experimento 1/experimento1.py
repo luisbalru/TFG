66e77b56de58f6ea71981b52de6c9001f6802b7e
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-04-27 15:34:56 +0200

    Experimento1

---
+++ Experimento 1/experimento1.py
@@ -0,0 +1,95 @@
+# SCRIPT PARA EL EXPERIMENTO 1
+# Determinar qué plano es más determinante en la clasificación
+# Tratamiento por slices
+# Author: Luis Balderas Ruiz
+
+
+
+from subjects import Sujeto
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+pacientes = []
+imagenes_pd = os.listdir('./Datos/PD')
+# Set the parameters by cross-validation
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': [1, 10, 100, 1000]},
+                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+scores = ['recall']
+
+
+print("LECTURA DE IMAGENES")
+
+print("Leyendo pacientes enfermos")
+
+for i in range(len(imagenes_pd)):
+    imagenes_pd[i] = './Datos/PD/' + imagenes_pd[i]
+    paciente = Sujeto(imagenes_pd[i],0)
+    pacientes.append(paciente)
+
+imagenes_control = os.listdir('./Datos/Control')
+
+print("Leyendo pacientes control")
+
+for i in range(len(imagenes_control)):
+    imagenes_control[i] = './Datos/Control/' + imagenes_control[i]
+    paciente = Sujeto(imagenes_control[i],1)
+    pacientes.append(paciente)
+
+print("X (Plano 0)")
+
+
+X_shape = pacientes[0].get_shape(0)
+accuracy_Xslices = []
+
+for j in range(X_shape):
+    dataset = []
+    target = []
+    np.random.shuffle(pacientes)
+    for i in range(len(pacientes)):
+        slice = pacientes[i].get_slice(0,j)
+        label = pacientes[i].get_label()
+        row = pacientes[i].get_wave2D(slice,'bior3.3',2)
+        dataset.append(row)
+        target.append(label)
+
+    X_train, X_test, y_train, y_test = train_test_split(dataset,target,test_size=0.3,random_state = 77145416)
+    pca = PCA(0.95)
+    pca.fit(X_train)
+    X_train = pca.transform(X_train)
+    X_test = pca.transform(X_test)
+    clf = 0
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(SVC(), tuned_parameters, cv=10,
+                           scoring='%s_macro' % score)
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+
+    mean_score_slice = 0
+    for k in range(0,10):
+        indices = np.random.choices(len(dataset),size=len(dataset),replace=False)
+        dataset_shuffle = X_train[indices]
+        target_shuffle = y_train[indices]
+        svm = SVC(C = clf.best_params_['C'],gamma = clf.best_params_['gamma'])
+        svm.fit(dataset_shuffle,target_shuffle)
+        y_true, y_pred = y_test, clf.predict(X_test)
+        mean_score_slice = mean_score_slice + accuracy_score(y_true,y_pred)
+
+    mean_score_slice = mean_score_slice/10
+    accuracy_Xslices.append(mean_score_slice)
+
+print(accuracy_Xslices)
